802cf485bf6d04b51e7cacf705f63450f0e90b7c
Akhil123454321 <akhilskasturi@outlook.com>
2023-10-20 23:04:42 -0400

    dashboard basic outline

--- app/src/pages/Dashboard.js
+++ app/src/pages/Dashboard.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Dashboard() {
+  const [groups, setGroups] = useState([
+    { name: 'Group 1' },
+    { name: 'Group 2' },
+    { name: 'Group 3' },
+  ]);
+
+  const addGroup = () => {
+    const groupName = prompt('Enter group name:');
+    if (groupName) {
+      setGroups([...groups, { name: groupName }]);
+    }
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <h1>My Groups</h1>
+      <ul>
+        {groups.map((group) => (
+          <li key={group.name}>{group.name}</li>
+        ))}
+      </ul>
+      <button onClick={addGroup}>Add Group</button>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
