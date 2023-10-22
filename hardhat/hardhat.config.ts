91077db066ed3cd4a60a2158e6709ed546216160
Vincent Palmerio <vpalmerio@protonmail.com>
2023-10-22 11:56:11 -0400

    Added network deploy config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+const dotenv = require("dotenv");
+dotenv.config();
+const { SEPOLIA_PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia-rpc.scroll.io/` || "",
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+    mantle: {
+      url: `https://rpc.testnet.mantle.xyz` || "",
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
+  
 };
 
 export default config;
